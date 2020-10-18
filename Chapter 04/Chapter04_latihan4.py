67bfc9d3626469dab579a5ee1a0da115df28eb0a
elhamsyahrianputra <72007074+elhamsyahrianputra@users.noreply.github.com>
2020-10-18 20:32:23 +0800

    Create Chapter04_latihan4.py
    script Chapter 04 Latihan no. 4

---
+++ Chapter 04/Chapter04_latihan4.py
@@ -0,0 +1,23 @@
+# Jarak yang ditempuh pak Amir dari kota A ke kota B adalah 125 km dengan kecepatan 62km/jam
+jarak1 = 125
+kecepatan1 = 62
+
+# Waktu yang di tempuh pak amir dari kota A ke kota B dalam menit
+waktu1 = jarak1//kecepatan1*60
+
+#Jarak kota B ke kota C adalah 256 km dengan kecepatan 70km/jam
+jarak2 = 256
+kecepatan2 = 70
+
+#waktu yang di tempuh pak amir dari kota B ke kota C dalam menit
+waktu2 = jarak2//kecepatan2
+
+# Saat sampai di kota B, pak Amir istirahat selama 45 menit
+istirahat = 45
+
+# total waktu yang ditempuh pak amir dari kota A ke kota C
+print("Total waktu yang di tempuh pak Amir dari kota A ke kota C adalah", waktu1+istirahat+waktu2, "menit")
+
+# Pak amin sampai pada pukul
+print("Maka pak Amir akan sampai di kota C pada pukul 08.48")
+
