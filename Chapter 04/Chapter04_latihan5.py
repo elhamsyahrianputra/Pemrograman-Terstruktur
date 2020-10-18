26ffbce672ad0fde419cac97e56108832bbda936
elhamsyahrianputra <72007074+elhamsyahrianputra@users.noreply.github.com>
2020-10-18 20:32:28 +0800

    Create Chapter04_latihan5.py
    script Chapter 04 Latihan no. 5

---
+++ Chapter 04/Chapter04_latihan5.py
@@ -0,0 +1,10 @@
+# Jumlah mahasiswa laki-laki PTIK adalah 57
+lakilaki = int(input("Berapa jumlah mahasiswa laki-laki PTIK....? "))
+
+# Jumlah mahasiswa perempuan PTIK adalah 24
+perempuan = int(input("Berapa jumlah mahasiswa perempuan PTIK....? "))
+
+#print diagram
+
+print("Laki-laki     :", "*"*lakilaki,"(",lakilaki,")")
+print("Perempuan     :", "*"*perempuan,"(",perempuan,")")
