71ebd982bcc014bde5ce16604e4ff298c75e858e
elhamsyahrianputra <72007074+elhamsyahrianputra@users.noreply.github.com>
2020-10-18 20:32:11 +0800

    Create Chapter04_latihan2.py
    script Chapter 04 Latihan no. 2

---
+++ Chapter 04/Chapter04_latihan2.py
@@ -0,0 +1,7 @@
+# Jarak kota A ke kota C adalah 795 km
+jarak = 795
+
+# Untuk 1 liter bensin bisa menempuh jarak 12 km
+tempuh = 12
+
+print("Banyak bensin yang dibutuhkan pak Budi dalam perjalanan tersebut adalah", (jarak/tempuh), "Liter")
