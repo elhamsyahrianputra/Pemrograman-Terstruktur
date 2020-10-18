77a7902d60e0868fa97da11a9f4375fdead2385d
elhamsyahrianputra <72007074+elhamsyahrianputra@users.noreply.github.com>
2020-10-18 20:32:18 +0800

    Create Chapter04_latihan3.py
    script Chapter 04 Latihan no. 3

---
+++ Chapter 04/Chapter04_latihan3.py
@@ -0,0 +1,6 @@
+# Bensin yang dibutuhkan Pak budi adalah 66.25
+bensin = 66.25
+# kapasitas tanki Pak Budi 50 Liter
+tanki = 50
+
+print("Banyaknya pak Budi mengisi bensin untuk perjalanan tersebut adalah", (bensin//tanki), "kali")
