b602f4712fe75edd7fa556bd6906b206101dfc13
elhamsyahrianputra <72007074+elhamsyahrianputra@users.noreply.github.com>
2020-11-03 00:26:01 +0800

    Create Chapter05_Latihan1_No.1.py

---
+++ Chapter 05/Chapter05_Latihan1_No.1.py
@@ -0,0 +1,14 @@
+# Input Nilai Ujian
+bahasa = int(input("Masukkan Nilai Bahasa Indonesia :  "))
+ipa = int(input("Masukkan Nilai Ipa 				:  "))
+mtk = int(input("Masukkan Nilai Matematika 		:  "))
+
+
+#Syrat Kelulusan
+if (bahasa >= 0 and bahasa <= 100) and (ipa >= 0 and ipa <= 100) and (mtk >= 0 and mtk <= 100):
+
+	if (bahasa >= 60) and (ipa >= 60) and (mtk > 70):
+		print("Status Kelulusan  				: LULUS")
+	else:
+		print("Status Kelulusan  				: TIDAK LULUS")
+
